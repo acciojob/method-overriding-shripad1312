48ec78f1b08575cfdc056b823888ef55d9e407b5
despo <shhripadb.20@gmail.com>
2023-09-10 23:44:53 +0530

    first submission using git

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
+
+class A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A{
+    @Override
+    String meth(){
+        return "Invoking method from class B";
+    }
+}
 public class Main {
+
+
+    public static void main(String[] args) {
+       B b=new B();
+       b.meth();
+
+    }
   
 }
